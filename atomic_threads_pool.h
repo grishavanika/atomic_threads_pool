a53dfb0d10a0ec8b6b7cb2803fd34710aff3bb55
grishavanika <grish.vanika@gmail.com>
2021-03-23 18:24:30 +0200

    refer to talk from the code.

--- atomic_threads_pool.h
+++ atomic_threads_pool.h
@@ -1,3 +1,7 @@
+// Implementation of threads pool with "Single-Thread Queue" idea
+// from CppCon 2015: Fedor Pikus PART 2 "Live Lock-Free or Deadlock (Practical Lock-free Programming)"
+// talk : https ://youtu.be/1obZeHnAwz4.
+
 #pragma once
 
 #include <atomic>
